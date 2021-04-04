e5ecbb188df826ef2d951f49df5487bbdb592931
haneenbader <haneenalwatan993@gmail.com>
2021-04-04 15:45:14 +0300

    added code

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let userName = prompt("what is your name" );
+console.log (typeof userName);
+
+let userAge = prompt("what is your age" );
+console.log (typeof userAge);
+
+let userLocation = prompt("where do you live ?" );
+console.log (typeof userLocation);
+
+let userSection = prompt("What section do you want to see ? , Excution , Quantity Surveying , Design Section" );
+console.log (typeof userSection);
+
+alert ("  welcome  " + userName + "  your age is  " + userAge + "  you live in  " + userLocation + "  and you want to see the  " + userSection );
